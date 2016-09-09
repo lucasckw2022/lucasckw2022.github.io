63201c33676c49370d7d46d8e4dbcb01a4b5a749
yeeie201 <lucas20229763@hotmail.com>
2016-09-08 19:23:53 -0700

    updated home and introduction

---
+++ js/images.js
@@ -0,0 +1,16 @@
+var introduction = {
+  "technology": [
+    ['React','react'],
+    ['Javascript','js'],
+    ['JQuery','jquery'],
+    ['Ruby','ruby'],
+    ['Ruby on Rails','rails'],
+    ['PostgreSQL','postgresql'],
+    ['GitHub','github'],
+    ['HTML','html'],
+    ['CSS','css'],
+    ['Sass','sass'],
+    ['Bootstrap','bootstrap'],
+    ['Materialize','materialize']
+  ]
+}
--- js/main.js
+++ js/main.js
@@ -1,11 +1,43 @@
 $(document).ready(function() {
     $('#content').fullpage({
-      fadingEffect: true,
       responsiveHeight: 1,
-      afterLoad: function( anchorLink, index){
+      afterLoad: (anchorLink, index)=>{
         if(index == 2){
-          $("div.hidden").fadeIn()
+          $('.introduction .card').fadeIn().toggleClass("hidden")
+          setTimeout(()=>{
+            $('.tech-skill p').after(
+            '<div class="techStack col s12"></div>'
+            );
+            var delay=0;
+            introduction.technology.map((item)=>{
+              var content = '<div class="col s3 hidden '+item[1]+'"><img src="images/introduction/'+ item[1] +'.png" /><p>'+ item[0] +'</p></div>';
+              setTimeout(()=>{$('.techStack').append(content);
+              hideElement("."+item[1])},delay+=100)
+            })
+          },300)
+        }
+        else if (index == 3){
+          hideElement("div.project")
+        }
+      },
+      onLeave: (index, nextIndex, direction)=>{
+        if(index == 2){
+          $('.introduction .card').fadeOut().toggleClass("hidden")
+          $('.techStack').fadeOut().remove();
+        } else if (index == 3){
+          $('.project .card').fadeIn().toggleClass("hidden")
         }
       }
     });
+
+    $('.title').textillate();
+
 });
+
+var hideElement = (tag)=>{
+  if($(tag).hasClass("hidden")){
+    $(tag).fadeIn().toggleClass("hidden")
+  } else {
+    $(tag).fadeOut().toggleClass("hidden")
+  }
+}
